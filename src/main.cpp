9f4d928207c531b932d40cda9e5746ab48e5cad1
hagen <hagen.weisse@stud.htwk-leipzig.de>
2020-07-17 18:09:06 +0200

    add conan dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <fstream>
 
 #include "format.h"
 
+#include "boost/filesystem.hpp"
 #include "csv.h"
 
 csv::table get_table(int argc, char *argv[]) {
     if (argc < 2) {
+        std::cerr << "Using stdin for input." << std::endl;
         return csv::read(std::cin);
     } else {
+        boost::filesystem::path p(argv[1]);
+        if (!boost::filesystem::exists(p)) {
+            std::cout << "File: " << argv[1] << "does not exist" << std::endl;
+            std::exit(1);
+        }
         std::ifstream input(argv[1]);
         return csv::read(input);
     }
